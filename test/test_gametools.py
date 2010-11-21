8eed22ee89f0d4d3ae9b81d5827c5e6ff9adfcad
Nolan Waite <nolan@nolanw.ca>
2010-11-21 14:05:43 -0700

    Switch to assertEqual when appropriate.

--- test/test_gametools.py
+++ test/test_gametools.py
@@ -9,18 +9,19 @@ class TestAddingAndRemovingPlayers(unittest.TestCase):
     
     def test_add_player_to_unstarted_game(self):
         gametools.add_player_named(self.game, 'testmanican')
-        self.assertTrue(len(self.game['players']) == 1, self.game)
+        self.assertEqual(len(self.game['players']), 1, self.game)
         gametools.add_player_named(self.game, 'testvetica')
-        self.assertTrue(len(self.game['players']) == 2, self.game)
+        self.assertEqual(len(self.game['players']), 2, self.game)
     
     def test_remove_player_from_unstarted_game(self):
         gametools.add_player_named(self.game, 'testmanican')
         gametools.add_player_named(self.game, 'testvetica')
         gametools.remove_player_named(self.game, 'testmanican')
-        self.assertTrue(len(self.game['players']) == 1, self.game)
-        self.assertTrue(self.game['players'][0]['name'] == 'testvetica')
+        self.assertEqual(len(self.game['players']), 1, self.game)
+        self.assertEqual(self.game['players'][0]['name'], 'testvetica')
     
     def test_add_player_to_started_game(self):
+        gametools.add_player_named(self.game, 'chumpwoman')
         gametools.start_game(self.game)
         with self.assertRaises(gametools.GameAlreadyStartedError):
             gametools.add_player_named(self.game, 'chumpman')
@@ -50,7 +51,7 @@ class TestGameSetup(unittest.TestCase):
     
     def test_tile_racks(self):
         for player in self.game['players']:
-            self.assertTrue(len(player['rack']) == 6)
+            self.assertEqual(len(player['rack']), 6)
     
     def test_tilebag(self):
         tilebag_should_be = sorted([str(i) + a for i in range(1, 13) 
@@ -58,9 +59,9 @@ class TestGameSetup(unittest.TestCase):
         for player in self.game['players']:
             for tile in player['rack']:
                 tilebag_should_be.remove(tile)
-        self.assertTrue(tilebag_should_be == sorted(self.game['tilebag']))
         for tile in self.game['lonely_tiles']:
             tilebag_should_be.remove(tile)
+        self.assertEqual(tilebag_should_be, sorted(self.game['tilebag']))
     
 
 if __name__ == '__main__':
