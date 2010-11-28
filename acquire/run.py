bb30efd136e74d8734cbc53e5b99394fcf3e1474
Nolan Waite <nolan@nolanw.ca>
2010-11-27 19:24:51 -0700

    run.py now starts the Mongrel2 handler as well.

--- acquire/run.py
+++ acquire/run.py
@@ -1,4 +1,5 @@
-# Start up a backend and a NetAcquire frontend.
+# Start up a backend and two frontends: one for NetAcquire, one for HTTP 
+# clients via Mongrel2.
 # This script is meant to quickly get up and running, so I'm ok with the path 
 # mangling going on here on a failed import.
 
@@ -9,6 +10,7 @@ except ImportError:
     import os, sys
     path_here = os.path.dirname(os.path.realpath(__file__))
     sys.path.insert(0, os.path.realpath(os.path.join(path_here, '../')))
+    sys.path.insert(1, os.path.realpath(os.path.join(path_here, '../lib')))
 from acquire.backend import Backend
 from acquire.netacquire import NetAcquire
 
@@ -17,6 +19,9 @@ settings = {
     'push_address': 'tcp://127.0.0.1:27183',
     'netacquire_address': '127.0.0.1:31415',
     'netacquire_name': 'Acquire',
+    'mongrel2_sender_id': 'd693a7cc-2bba-469a-b478-11a50ca09116',
+    'mongrel2_send_spec': 'tcp://127.0.0.1:9999',
+    'mongrel2_recv_spec': 'tcp://127.0.0.1:9998',
 }
 
 back = Backend()
@@ -24,7 +29,7 @@ back_settings = {
     'pub_address': settings['pub_address'],
     'pull_address': settings['push_address'],
 }
-back_thread = Thread(target=back.run, kwargs=back_settings)
+back_thread = Thread(target=back.run, name='backend', kwargs=back_settings)
 front = NetAcquire()
 accept_address = settings['netacquire_address'].split(':')
 accept_address = (accept_address[0], int(accept_address[1]))
@@ -34,10 +39,22 @@ front_settings = {
     'backend_push_address': settings['push_address'],
     'accept_address': accept_address,
 }
-front_thread = Thread(target=front.run, kwargs=front_settings)
+front_thread = Thread(target=front.run, name='netacquire', 
+                      kwargs=front_settings)
+def http():
+    from acquire.http import Mongrel2Handler
+    h = Mongrel2Handler(settings['mongrel2_sender_id'], 
+                        send_spec=settings['mongrel2_send_spec'], 
+                        recv_spec=settings['mongrel2_recv_spec'])
+    h.run(backend_sub_address=settings['pub_address'], 
+          backend_push_address=settings['push_address'])
+http_thread = Thread(target=http, name='http')
 
 back_thread.start()
 front_thread.start()
+http_thread.start()
 back_thread.join()
 if front_thread.is_alive():
     front_thread.join()
+if http_thread.is_alive():
+    http_thread.join()
